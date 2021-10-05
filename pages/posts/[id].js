c86ebbc72b4ae61efd599c3a0e98c6fc25e98c08
im-coder-lg <balovijayagan@gmail.com>
2021-10-05 05:11:18 +0000

    Almost dome till Dynamic Routes - 5

---
+++ pages/posts/[id].js
@@ -0,0 +1,32 @@
+import Layout from '../../components/layout'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export async function getStaticProps() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
