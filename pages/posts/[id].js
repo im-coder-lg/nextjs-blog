1e265c7bd01235387e2aaed2d79a10f705fe8127
im-coder-lg <balovijayagan@gmail.com>
2021-10-07 03:58:40 +0000

    Done until Dynamic Routes - Part 7

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,19 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
     </Layout>
   )
 }
@@ -15,7 +21,7 @@ export default function Post({ postData }) {
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
