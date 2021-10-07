ee4c4dcbf1512ca7141c5536b9f9e695cb9d333e
im-coder-lg <balovijayagan@gmail.com>
2021-10-07 04:24:09 +0000

    Finish Dynamic Routes Part 8

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Date from '../components/date'
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
@@ -22,11 +23,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {title}
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
             <br />
-            {id}
-            <br />
-            {date}
+            <small className={utilStyles.lightText}>
+              <Date datestring={date} />
+            </small>
           </li>
         ))}
         </ul>
